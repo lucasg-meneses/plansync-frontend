cfc6231ec96f0b201093687c68ee1963d1d5263f
lucasg-meneses <lucasgmeneses@protonmail.com>
2024-01-24 20:21:56 -0400

    init projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
-  title = 'plansync-frontend';
+  title : String = 'PlanSync';
+  author: String = 'Lucas G. Meneses'
 }
